daa01c8bff4e88cb135863c02cab14f4faddc175
Alan García <ing.alangaro@gmail.com>
2023-01-24 22:02:36 -0600

    Aplicación básica para consultar Gifs

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.Interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey: string = '1XoTQGkRoOOI4ogO1PjwaaTNvdtB0YSb';
+  private _historial: string[] = [];
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  public resultados: Gif[] = [];
+
+  get historial() {
+    return [...this._historial]
+  }
+
+  constructor( private http: HttpClient ) { 
+    this._historial = JSON.parse( localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse( localStorage.getItem('resultados')!) || [];
+  }
+  
+  buscarGifs( query: string = '') {
+    query = query.trim().toLowerCase();
+    if( !this._historial.includes(query) ){
+      this._historial.unshift( query );
+    }
+    this._historial = this._historial.splice(0,10);
+    console.log(this._historial);
+
+    localStorage.setItem('historial', JSON.stringify(this._historial) )
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', query);
+
+    console.log(params.toString());
+
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params } )
+    .subscribe( ( resp ) => {
+      console.log(resp.data);
+      this.resultados = resp.data;
+      localStorage.setItem('resultados', JSON.stringify(resp.data));
+    } )
+  }
+
+  borrarBusqueda(){
+    this.resultados = [];
+    this._historial = [];
+    localStorage.removeItem('historial');
+    localStorage.removeItem('resultados');
+  }
+}
